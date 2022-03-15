062912b3c8c677491aa5f224538e4974d79e6d8b
Tenghui Jiang <jthivan@gmail.com>
2022-03-15 13:16:09 +0800

    111

--- src/todo/web-interview/test.js
+++ src/todo/web-interview/test.js
@@ -1,12 +1,24 @@
 
-const currying = fn => {
-    const len = fn.length
-    return function curr (...args1) {
-        if (args1.length >= len) {
-            return fn(...args1)
-        }
-        return (...args2) => curr(...args1, ...args2)
-    }
+for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(new Date(), i);
+    }, 1000);
 }
 
-currying()
+
+// 经典面试题：循环中使用闭包解决 var 定义函数的问题
+// 第一种是使用闭包的方式
+// for (var i = 0; i < 5; i++) {
+//     (function (j) {
+//         setTimeout(function timer() {
+//             console.log(j)
+//         }, j * 1000)
+//     })(i)
+// }
+
+// 第三种就是使用 let 定义 i 了来解决问题了，这个也是最为推荐的方式
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(function() {
+//         console.log(i)
+//     }, i * 1000)
+// }
